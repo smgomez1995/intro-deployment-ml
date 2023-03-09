5166209194a38db432d27ac977d9e88dbbb1384e
Sebastian gomez <gomez.sebastian@pucp.pe>
2023-03-08 23:52:06 -0500

    testing pero feo

--- api/app/models.py
+++ api/app/models.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 
 class PredictionRequest(BaseModel):
-    opening_groos : float
+    opening_gross : float
     screens: float
     production_budget: float
     title_year: int
